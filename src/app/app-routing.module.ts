97bb59995f3a3dd068271b941b851d1b2746e9b8
Ziquan Miao <ziquanmiao@Ziquans-MacBook-Pro.local>
2021-09-14 14:03:46 -0400

    add initial components setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ConfirmComponent } from './components/confirm/confirm.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'confirm', component: ConfirmComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  // { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ConfirmComponent } from './components/confirm/confirm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    CheckoutComponent,
+    ConfirmComponent
   ],
   imports: [
     BrowserModule,
